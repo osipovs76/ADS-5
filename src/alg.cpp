4cc4e05fda71f7502a2e49117a3b2a339ca056fb
Osipov Andrey(21-IST5) <94702769+osipovs76@users.noreply.github.com>
2022-04-03 23:21:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,47 +1,4 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_TSTACKH
-#define INCLUDE_TSTACKH
-
-template<typename T, int size>
-class TStack {
- private:
-  T arr[100];
-  int top;
-
- public:
-  TStack() : top(-1) {}
-  bool isEmpty() const {
-    return top == -1;
-  }
-
-  bool isFull() const {
-    return top == size - 1;
-  }
-
-  T get() {
-    if (!isEmpty())
-      return *(arr + top);
-    else
-      throw "Stack is empty yet, add more elements";
-  }
-
-  void pop() {
-    if (!isEmpty())
-      --top;
-  }
-
-  void push(T value) {
-    if (!isFull())
-      *(arr + ++top) = value;
-    else
-      throw "Stack is already full, remove some elements";
-  }
-};
-#endif  // INCLUDE_TSTACKH
-
-TStack<char, 100> stack1;
-TStack<int, 100> stack2;
-
 #include <string>
 #include <map>
 #include "tstack.h"
