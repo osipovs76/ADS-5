db17540d339bdcc6d477f54b618cfa25cdec7fa2
Osipov Andrey(21-IST5) <94702769+osipovs76@users.noreply.github.com>
2022-04-03 23:18:42 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,6 +4,40 @@
 
 template<typename T, int size>
 class TStack {
+ private:
+  T arr[100];
+  int top;
 
+ public:
+  TStack() : top(-1) {}
+  bool isEmpty() const {
+    return top == -1;
+  }
+
+  bool isFull() const {
+    return top == size - 1;
+  }
+
+  T get() {
+    if (!isEmpty())
+      return *(arr + top);
+    else
+      throw "Stack is empty yet, add more elements";
+  }
+
+  void pop() {
+    if (!isEmpty())
+      --top;
+  }
+
+  void push(T value) {
+    if (!isFull())
+      *(arr + ++top) = value;
+    else
+      throw "Stack is already full, remove some elements";
+  }
 };
-#endif  // INCLUDE_TSTACK_H_
+#endif  // INCLUDE_TSTACKH
+
+TStack<char, 100> stack1;
+TStack<int, 100> stack2;
